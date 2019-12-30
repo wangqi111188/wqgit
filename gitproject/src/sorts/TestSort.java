e7a2bb176ffe608d32dfcd2ef3db8e3fb0958d54
wangqi111188 <wangqi111188@163.com>
2019-12-30 21:34:21 +0800

    冒泡，选择排序

--- gitproject/src/sorts/TestSort.java
+++ gitproject/src/sorts/TestSort.java
@@ -12,6 +12,9 @@ public class TestSort {
 		int[] arr = generate(10);
 		maoPaoSort(arr);
 		System.out.println(Arrays.toString(arr));
+		arr=generate(8);
+		selectSort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 	/**
 	 * 冒泡排序的实现方法
@@ -29,7 +32,40 @@ public class TestSort {
 			}
 		}
 	}
+	/**
+	 * 选择排序
+	 * @param arr
+	 */
+	public static void selectSort(int[] arr){
+		int temp=0;
+		int k=-1;
+		for(int i=0;i<arr.length;i++){
+			temp=arr[i];
+			for(int j=i;j<arr.length;j++){
+				
+				if(temp>arr[j]){
+					temp=arr[j];
+					k=j;
+				}
+			}
+			if(k!=-1){
+				arr[k]=arr[i];
+				arr[i]=temp;
+			}
+		}
+		
+		
+		
+		
+	}
 	
+	
+	
+	/**
+	 * 
+	 * @param m 数组元素个数
+	 * @return 生成m个随机整型数组
+	 */
 	public static int[] generate(int m){
 		int[] arr =  new int[m];
 		for (int i = 0; i < arr.length; i++) {
